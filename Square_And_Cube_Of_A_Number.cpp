6767ceabe328a640ee7eb97f84c9d4d4ffff3645
MaryCode009 <learnwithme805@gmail.com>
2025-05-19 23:40:08 +0530

    Update and rename 11.P_square&cubeOfNo.userInput_useCin.cpp to Square_And_Cube_Of_A_Number.cpp

--- Square_And_Cube_Of_A_Number.cpp
+++ Square_And_Cube_Of_A_Number.cpp
@@ -1,15 +1,11 @@
-//1.find square of a number. Take user input.
+//1.find square and cube of a number. Take input from user.
 /*in user input we can give new value without changing anything in code e.g, In calculator we keep calculating new values
- without changing anything in code (remember google example)*/
+ without changing anything in code */
 
 #include<iostream>
 using namespace std;
 
 int main(){
-int a=5; //this is input not user input.
-cout<<a<<endl;
-
-
 int num;
 cin>>num; // taking input from user (user input). User can keep changing his input in console as per his requirements
 cout<<"square of the given number = "<< num*num<<endl<<endl;
