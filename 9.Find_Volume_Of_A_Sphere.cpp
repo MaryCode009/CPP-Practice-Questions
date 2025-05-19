23620861ddcda6fb9a1cf369bc729e94d257c1ef
MaryCode009 <learnwithme805@gmail.com>
2025-05-19 23:28:48 +0530

    Update and rename 9.PH_volumeOfSphere.cpp to 9.Find_Volume_Of_A_Sphere.cpp

--- 9.Find_Volume_Of_A_Sphere.cpp
+++ 9.Find_Volume_Of_A_Sphere.cpp
@@ -1,18 +1,18 @@
 //find volume of sphere (v=(4*pi*r^3)/3.
-/*A sphere is a flat shape.  it is 2 dimentional 2D like a ring drawn on paper having only length and breadth while
+/*A sphere is a flat shape.  it is 2 dimentional (2D) like a ring drawn on paper having only length and breadth while
 sphere has 3 dimentions 3D like a basketball having lenth, breadth and height*/
 
 #include<iostream>
 using namespace std;
 
 int main(){
-   // one sphere
+   // sphere1
    float r=2.5; //radius
    float pi=3.1415;
    float volume=(4*pi*r*r*r)/3;
    cout<<"volume of Sphere1 = "<<volume<<endl<<endl;
 
-   //another sphere taking user input
+   //another sphere taking input from user
    float radius2, volumeOfSphere2;
    cout<<"enter the radius of this sphere2 :";
    cin>>radius2;
