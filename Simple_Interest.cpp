a74c3efbb9ddc942a4a571b0b9d84dff3fb02973
MaryCode009 <learnwithme805@gmail.com>
2025-05-19 23:44:31 +0530

    Update and rename Find_Simple_Interest.cpp to Simple_Interest.cpp

--- Simple_Interest.cpp
+++ Simple_Interest.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
  float p=1000;// Principle:main amount
- float r=30; // Rate:commission  (30%)
+ float r=30; // Rate: e.g, commission  (30%)
  float t=3; // Time:period of time to invest (3 years) 
- float si=(p*r*t)/100; //simple interest.       we should use braces in formulla(p*r*t) to apply BODMAS rule
+ float si=(p*r*t)/100; //simple interest.       we should use braces in formulla(p*r*t) to avoid conflicts
 cout<<si;
     
 return 0;
