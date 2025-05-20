eaacc5c58d04a731ef7357ae07ac763cdbd236e9
MaryCode009 <learnwithme805@gmail.com>
2025-05-20 21:35:20 +0530

    Add files via upload

---
+++ 16.P_Typecasting_halfOfInteger_conversions.cpp
@@ -0,0 +1,25 @@
+// Program to take integer x input and print half of the number
+/*half of a number can b decimal number also but due to its int datatype it will round off in case of decimal number so we
+need to typecast it (into float or double). typecast is coverting the type of a variable from one to another */
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=4, b=5;
+   cout<<a/2<<endl<<b/2<<endl; // output of b/2 will 2 which is not exact half
+
+   // Program Question
+   int x;
+   cout<<"enter the number\n";
+   cin>>x;
+   float y=(float)x;//typecasting
+   cout<<y/2<<endl;
+
+   //shorcut
+   int p=7;
+   cout<<(float)p/2;  //output3.5
+    
+
+return 0;
+}
