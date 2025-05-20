1acb25b63d388ac13bb88436fe79fc49a3f480f5
MaryCode009 <learnwithme805@gmail.com>
2025-05-20 23:12:07 +0530

    Update and rename 22.P_Cond_ifElse_EvenOdd.cpp to Given_Number_is_Even_or_Odd.cpp

--- Given_Number_is_Even_or_Odd.cpp
+++ Given_Number_is_Even_or_Odd.cpp
@@ -1,5 +1,5 @@
 //Program to take a positive integer input and tell if it is even or odd
-//If else deals with statements which have some conditions and the output depends on that condition or situation
+//If else deals with conditional statements and the output depends on that condition
 #include<iostream>
 using namespace std;
 
@@ -8,17 +8,16 @@ int main(){
    cout<<"enter the number\n";
    cin>>num;
    
-if(num%2==0){ //num%2==0 codition for even , == equality sign . if this condition is true it will execute otherwise else part
+if(num%2==0){//num%2==0 is codition for even(a number divisible by 2 is even).If this condition is true it will print even 
    cout<<"the number is even\n";
 }
-else {
-    cout<<"the number is odd\n" ;
+else {      //if above condition is not satisfied, then  the number is odd. so else part will print odd
+    cout<<"the number is odd\n" ; 
     cout<<"means not divisible by 2\n"; // this is part of else as it is inside { } of else
 }
   cout<<"Done"; //this is part of whole code as it is outside of if and else part
 
-   //if(num%2!=0)  //num%2!=0 is codition for odd  And != is not equal sign
-   //cout<<"the number is odd\n";
+   
 
    
    
