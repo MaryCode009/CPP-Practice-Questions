a21e9f0c1a4b240c63391d82251e11cee6b42bd4
MaryCode009 <learnwithme805@gmail.com>
2025-05-21 23:14:10 +0530

    Add files via upload

---
+++ 23.PH_DivisibleBy5_relationalOperators.cpp
@@ -0,0 +1,18 @@
+//Program to take a positive integer input and tell if it is divisible by 5 or not
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<"enter the number \n";
+   cin>>n;
+   // Relational operators: >  <  <=  >=  !=  ==  
+if(n%5==0){
+      cout<<"the given number is divisible by 5\n";
+   }
+else{
+      cout<<"the given number is not divisible by 5\n";
+}
+   
+return 0;
+}
